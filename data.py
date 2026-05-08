8e3fd81e848dc703d6a7a995cec09f53d1dfcd9b
Daniel Price <dcprice98@gmail.com>
2026-05-08 00:17:54 -0500

    started work on larvae growth

--- data.py
+++ data.py
@@ -17,11 +17,13 @@ def maintain_all(dt):
     total_consumption = 0
     total_production = 0
     queen.produce(dt)
+    larvae.produce(dt)
     total_production = forager.amount * forager.efficiency
     for ant_type in Population.ant_types:
         total_consumption -= ant_type.consumption * ant_type.amount
     food.inc_dec_amount(total_production - abs(total_consumption))
     print(larvae.amount % 1)
+    print(forager.amount)
 
 
 class Resource():
@@ -75,7 +77,7 @@ class Population(Resource):
         Population.capacity -= abs(dec_amount)
 
 class Queen(Population):
-    def __init__(self, name, amount=1, consumption=.05, efficiency=.1):
+    def __init__(self, name, amount=1, consumption=.05, efficiency=.2):
         super().__init__(name, amount, consumption, efficiency)
     
     def produce(self, dt):
@@ -85,19 +87,31 @@ class Queen(Population):
             larvae.add_res(self.efficiency * dt)
 
 class Larvae(Population):
-    def __init__(self, name, amount=0, consumption=.005):
+    def __init__(self, name, amount=0, consumption=.005, efficiency=1):
         super().__init__(name, amount, consumption)
+        self.efficiency = efficiency
+        self.ant_limits = {'forager': 10, 'nursery': 0, 'tunneller': 0, 'soldier': 0}
+
+
+    def update_ant_limites():
+        pass
 
     def produce(self, dt):
-        pass
+        larvae_change = self.efficiency * dt
+        if larvae.amount > 0:
+            if forager.get_amount() < self.ant_limits['forager']:
+                larvae.amount -= larvae_change
+                forager.amount += larvae_change
+
 
 class Nursery(Larvae):
     def __init__(self, name, amount=0, consumption=.02):
         super().__init__(name, amount, consumption)
 
 class Forager(Population):
-    def __init__(self, name, amount=0, consumption=.2, efficiency=.21):
+    def __init__(self, name, limit=10, amount=0, consumption=.2, efficiency=.21):
         super().__init__(name, amount, consumption, efficiency)
+        self.limit = limit
 
     def produce(self, dt):
         food.add_res(self.amount * (self.efficiency * dt))
